bc6e51a911478ed247925b56c70b91c44cb7bed1
滝澤宏樹 <takizawahiroki@takizawahirokinoMacBook-Air-8.local>
2026-02-10 22:29:16 +0900

    clasp連携設定完了：プロジェクトをAnalyticsとInportに分離

--- Form/templates.js
+++ Form/templates.js
@@ -96,12 +96,12 @@ export const formHTML = `
           /* 🍎 選択肢に「関連リンク」を追加し、入力欄にURLとタイトルのセット（2件分）を用意 */
 <div class="lz-choice-flex">
   <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="home"><span class="lz-choice-inner">HP</span></label>
-  <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="ec"><span class="lz-choice-inner">通販</span></label>
+  <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="ec"><span class="lz-choice-inner">ECサイト</span></label>
+  <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="rel"><span class="lz-choice-inner">関連リンク</span></label>
   <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="ig"><span class="lz-choice-inner">Instagram</span></label>
   <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="fb"><span class="lz-choice-inner">Facebook</span></label>
   <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="x"><span class="lz-choice-inner">X</span></label>
   <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="line"><span class="lz-choice-inner">LINE</span></label>
-  <label class="lz-choice-item lz-sub-choice-item"><input type="checkbox" name="sns_trigger" value="rel"><span class="lz-choice-inner">関連リンク</span></label>
 </div>
 <div id="sns-inputs" style="display:flex; flex-direction:column; gap:12px; margin-top:15px;">
   <div id="f-home" style="display:none;"><input type="url" name="url_home" class="lz-input" placeholder="HPのURL (https://...)"></div>
